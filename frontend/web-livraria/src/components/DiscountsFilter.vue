c8624e0f5ab12e5eb6723dcdb6ba364d79403768
Zanad <daniel.zanad.dz@gmail.com>
2022-01-29 13:45:58 -0300

    Adicionanando parte de filtros de desconto

---
+++ frontend/web-livraria/src/components/DiscountsFilter.vue
@@ -0,0 +1,51 @@
+<template>
+  <div id="discount-filters">
+    <div id="discount-filters-msg">
+      <h3>Livros e mais livros</h3>
+      <p>
+        Aqui você encontra as melhores opções em Livros, Games, Músicas e
+        Eletrônicos além de eventos gratuitos e as melhores ofertas. Confira!
+      </p>
+    </div>
+    <div id="discount-filters-btn">
+      <button class="button-filter">Mais vendidos</button>
+      <button class="button-filter">Destaques</button>
+      <button class="button-filter">Super ofertas</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+div#discount-filters-msg {
+  font-family: "Roboto", sans-serif;
+  margin-left: 40px;
+  font-size: 1.2em;
+}
+div#discount-filters-msg > h3 {
+  font-style: normal;
+  font-weight: bolder;
+  line-height: 35px;
+  margin-bottom: 5px;
+}
+div#discount-filters-btn{
+    margin: 40px 40px 40px 0px;
+}   
+.button-filter{
+    font-family: "Roboto", sans-serif;
+    margin-left: 40px;
+    padding: 15px;
+    font-size: 1.1em;
+    color: #6ab6cf;
+    border: solid;
+    border-radius: 10px;
+    cursor: pointer;
+}
+.button-filter:focus{
+    background-color: #7ad3f0;
+    color: white;
+}   
+</style>
--- frontend/web-livraria/src/views/Home.vue
+++ frontend/web-livraria/src/views/Home.vue
@@ -3,6 +3,7 @@
     <Header />
     <NavBar />
     <Discount />
+    <DiscountFilter />
   </div>
 </template>
 
@@ -11,9 +12,10 @@
 import Header from "@/components/Header.vue";
 import NavBar from "@/components/Nav.vue";
 import Discount from '@/components/Discounts.vue';
+import DiscountFilter from '@/components/DiscountsFilter.vue'
 
 export default {
   name: "Home",
-  components: { Header, NavBar, Discount },
+  components: { Header, NavBar, Discount, DiscountFilter },
 };
 </script>
